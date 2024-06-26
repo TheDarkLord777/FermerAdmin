512c2a2057f9428f35b11deff4a03a3bbc7768ec
MAX <muzaffarovmahmudbek@gmail.com>
2024-06-26 11:58:45 +0500

    lazy loading

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,13 +1,16 @@
-import Layout from "@layout/Layout.tsx";
-import NotFounded from "@pages/NotFounded/NotFounded.tsx";
-import Dashboard from "@pages/Dashboard/Dashboard.tsx";
+import React, { Suspense } from "react";
 import { AppProviders } from "@context/Provider.tsx";
-import Courses from "@pages/Courses/Courses.tsx";
-import Books from "@pages/Books/Books.tsx";
-import MeningTomorqam from "@pages/MeningTomorqam/MeningTomorqam.tsx";
-import Reports from "@pages/Reports/Reports.tsx";
-import Students from "@pages/Students/Students.tsx";
+import Layout from "@layout/Layout.tsx";
 
+const NotFounded = React.lazy(() => import("@pages/NotFounded/NotFounded.tsx"));
+const Dashboard = React.lazy(() => import("@pages/Dashboard/Dashboard.tsx"));
+const Courses = React.lazy(() => import("@pages/Courses/Courses.tsx"));
+const Books = React.lazy(() => import("@pages/Books/Books.tsx"));
+const MeningTomorqam = React.lazy(
+  () => import("@pages/MeningTomorqam/MeningTomorqam.tsx"),
+);
+const Reports = React.lazy(() => import("@pages/Reports/Reports.tsx"));
+const Students = React.lazy(() => import("@pages/Students/Students.tsx"));
 const routes = [
   {
     path: "/",
@@ -16,31 +19,59 @@ const routes = [
         <Layout />
       </AppProviders>
     ),
-    errorElement: <NotFounded />,
+    errorElement: (
+      <Suspense fallback={<p>Loading ...</p>}>
+        <NotFounded />
+      </Suspense>
+    ),
     children: [
       {
         path: "/",
-        element: <Dashboard />,
+        element: (
+          <Suspense fallback={<p>Loading...</p>}>
+            <Dashboard />
+          </Suspense>
+        ),
       },
       {
         path: "courses",
-        element: <Courses />,
+        element: (
+          <Suspense fallback={<p>Loading...</p>}>
+            <Courses />
+          </Suspense>
+        ),
       },
       {
         path: "books",
-        element: <Books />,
+        element: (
+          <Suspense fallback={<p>Loading...</p>}>
+            <Books />
+          </Suspense>
+        ),
       },
       {
         path: "mening-tomorqam",
-        element: <MeningTomorqam />,
+        element: (
+          <Suspense fallback={<p>Loading...</p>}>
+            <MeningTomorqam />
+          </Suspense>
+        ),
       },
       {
         path: "reports",
-        element: <Reports />,
+        element: (
+          <Suspense fallback={<p>Loading...</p>}>
+            <Reports />
+          </Suspense>
+        ),
       },
       {
         path: "students",
-        element: <Students />,
+        element: (
+          <Suspense fallback={<p>Loading...</p>}>
+            <Students />
+          </Suspense>
+        ),
       },
     ],
   },
